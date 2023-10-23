d69dde07caae6b2e12a0ab52e8ac0503486182ba
Arnaud Lafon <arnaud.lafon@proton.me>
2023-10-23 19:20:23 +0200

    read md files from directory

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/a-lafon/go-doc-serve/utils"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Starting the program")
+
+	rootDir := "/home/arnaud/Documents/doc"
+
+	markdownFiles, err := utils.GetMarkdownFiles(rootDir)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(markdownFiles)
+
+	fmt.Println("End of program")
 }
